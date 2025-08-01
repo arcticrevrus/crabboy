367055eaf0e1db834b18169125176633ce3df5b9
ArcticRevrus <arcticrevrus@gmail.com>
2025-08-01 14:58:33 -0500

    Updating main

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
-mod asm;
+use crate::memory::Memory;
+
 mod cartridge;
 mod cpu;
 mod memory;
 
 fn main() {
+    let mut memory = memory::MemoryMap::new();
+    let mut cpu = cpu::Cpu::new(cpu::Mode::DMG);
 }
